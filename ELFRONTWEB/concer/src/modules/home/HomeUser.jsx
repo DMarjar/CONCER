4e36f8eb7224c95c965319184653ebfee371412f
Ana2145 <20213tn149@utez.edu.mx>
2023-04-03 15:26:12 -0500

    algo

--- ELFRONTWEB/concer/src/modules/home/HomeUser.jsx
+++ ELFRONTWEB/concer/src/modules/home/HomeUser.jsx
@@ -1,7 +1,7 @@
 import { Col, Row } from "react-bootstrap";
 import React from "react";
 import { NavbarAll } from "../../shared/components/Navbar";
-import {AsideUser} from "../../shared/components/AsideUser";
+import { AsideUser } from "../../shared/components/AsideUser";
 
 
 
@@ -10,15 +10,18 @@ export const HomeUser = () => {
 
   return (
     <>
+    <Row className="m-0">
       <NavbarAll />
-      <Row>
+    </Row>
+      
+      <Row className="h-100 square border border-2 border-light " style={{ height: "100vh" }}>
         <Col
-          className="col-lg-2 col-md-3 col-sm-3 square border border-2 h-100 m-0"
-          style={{ height: "300px", position: "relative" }}
+          className="col-lg-2 col-md-3 col-sm-3 square border border-bottom-0 m-0"
+          style={{ height: "600px", position: "relative" }}
         >
-            <AsideUser/>   
+          <AsideUser/>
         </Col>
-        <Col className="col-lg-10 col-md-9 col-sm-9 square border border-2 m-0">
+        <Col className="col-lg-10 col-md-9 col-sm-9 square border border-bottom-0 m-0">
           {/* <Router>    
                      <Routes>
 
@@ -27,7 +30,7 @@ export const HomeUser = () => {
           contenido user
         </Col>
       </Row>
-      
+
     </>
   );
 };
